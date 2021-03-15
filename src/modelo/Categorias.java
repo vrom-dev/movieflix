1fa7e9f972a935ce7551c2bdab70ba4768d22cbb
Awesome <dmc995@gmail.com>
2021-03-15 15:04:21 +0100

    Clase Pelicula y ENUM Categorias - Revisar ENUM porque no sé si funciona como esperado

--- src/modelo/Categorias.java
+++ src/modelo/Categorias.java
@@ -1,5 +1,42 @@
 package modelo;
 
+/* @author Alvaro */ 
+
 public enum Categorias {
+	
+	Policiaca(1), 
+	Romantica(2), 
+	Aventuras(3),
+	Comedia(4),
+	Animacion(5),
+	Thriller(6);
+	
+	private final int codigo;
+	private static final int size = Categorias.values().length;
+	
+	private Categorias(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	private int getCodigo() {
+		return codigo;
+	}
+	
+	public static int getSize() {
+		return size;
+	}
+	
+	/* @author Antonio */ 
+    private static Categorias[] values = null;
+
+    public static Categorias getNombreCategoria(int val) {
+        if (Categorias.values == null) {
+            Categorias.values = Categorias.values();
+        }
+        
+        return Categorias.values[val];
+    }
+	
+	
 
 }
