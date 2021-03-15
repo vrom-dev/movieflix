7d2aaec179809c17dbce427d20ad59e62536ea3b
Vic Romero <vromerocomino@gmail.com>
2021-03-15 23:03:02 +0100

    Creada clase Usuario y clase Fechas

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,5 +1,82 @@
 package modelo;
+/**
+*
+* @author Tangerine (Víctor Romero)
+* @version 0.0.1
+*/
+import java.time.LocalDate;
+
+import utilidades.Fechas;
+import utilidades.LecturaDatos;
+
+/**
+* Crea una clase Usuario, con nombre, fecha de nacimiento
+* y ciudad.
+*/
 
 public class Usuario {
 
+	private String nombre;
+	private LocalDate fechaNacimiento;
+	private String ciudad;
+	
+	/** Constructores  */
+	
+	public Usuario() {
+	}
+	
+	public Usuario(String nombre, LocalDate fechaNacimiento, String ciudad) {
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.ciudad = ciudad;
+	}
+	
+	/** Getters y Setters  */
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	public String getCiudad() {
+		return ciudad;
+	}
+	
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	/** 
+	 * Método inicializa los atributos de cada Usuario.
+	 */
+	public void crearUsuario() {
+        try {
+            this.nombre = LecturaDatos.leerLinea("Introduce el nombre del usuario:");
+            this.fechaNacimiento = Fechas.generarFecha("Introduce la fecha de nacimiento (dd/MM/yyyy):",  "dd/MM/yyyy");
+            this.ciudad = LecturaDatos.leerLinea("Introduce su ciudad de residencia:");
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+	}
+	
+	/** 
+	 * Método que sobreescribe toString().
+	 */
+	@Override
+	public String toString() {
+		return "Nombre: " + nombre + " - Fecha de nacimiento: " + Fechas.formatearFecha(fechaNacimiento, "dd-MM-yyyy") + " Ciudad: " + ciudad;
+	}
+
+	
 }
