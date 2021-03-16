816db6956bb4f0289e0b737de3633071a54972f8
Vic Romero <vromerocomino@gmail.com>
2021-03-16 09:14:11 +0100

    Corregido un typo

--- src/utilidades/Fechas.java
+++ src/utilidades/Fechas.java
@@ -20,8 +20,8 @@ public class Fechas {
 	
 	/**
 	 * Genera un objeto LocalDate a partir de una fecha introducida por teclado y lo devuelve.
-	 * @param mensaje: mensaje que verá el usuario por consola para solicitarle la fecha.
-	 * @para formato: formato de fecha que tendrá que introducir el usuario (ej: "dd/MM/yyyy", "dd-MM-yyyy", "yyyy-MM-dd").
+	 * @param mensaje = mensaje que verá el usuario por consola para solicitarle la fecha.
+	 * @param formato = String con el formato de la fecha que tendrá que introducir el usuario (ej: "dd/MM/yyyy", "dd-MM-yyyy", "yyyy-MM-dd").
 	 */
 
 	public static LocalDate generarFecha(String mensaje, String formato) {
@@ -45,8 +45,8 @@ public class Fechas {
 	
 	/**
 	 * Recibe un Objeto tipo LocalDate y lo convierte a String, siguiendo el formato especificado.
-	 * @param fecha: objeto LocalDate que queremos convertir a String.
-	 * @para formato: formato de fecha que tendrá que seguirá la conversión (ej: "dd/MM/yyyy", "dd-MM-yyyy", "yyyy-MM-dd").
+	 * @param fecha = objeto LocalDate que queremos convertir a String.
+	 * @param formato = String con el formato de fecha que tendrá que seguirá la conversión (ej: "dd/MM/yyyy", "dd-MM-yyyy", "yyyy-MM-dd").
 	 */
 
 	public static String formatearFecha(LocalDate fecha, String formato) {
