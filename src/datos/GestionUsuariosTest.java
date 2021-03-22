0fb62f8465c6aa0ff255af41e268bae3af7e69ce
Alberto <alberto.sagi@gmail.com>
2021-03-22 11:48:51 +0100

    JUnit test del método eliminarUsuario

--- src/datos/GestionUsuariosTest.java
+++ src/datos/GestionUsuariosTest.java
@@ -4,27 +4,40 @@ import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import modelo.Usuario;
 
+/**
+ * @author VICTOR
+ *
+ */
 public class GestionUsuariosTest {
 	GestionUsuarios listaUsuarios = new GestionUsuarios();
-	
-	@BeforeClass
+
+	@Before
 	public void crearListaUsuarios() {
 		Usuario user2 = new Usuario("Luis", LocalDate.now(), "Madrid");
 		listaUsuarios.altaUsuarios(1, user2);
 	}
-	
+
 	@Test
 	public void testAltaUsuarios() {
 		Usuario user = new Usuario("Victor", LocalDate.now(), "Barcelona");
 		boolean resultado = listaUsuarios.altaUsuarios(2, user);
 		assertTrue(resultado);
 	}
-	
-	
+
+	/**
+	 * Test para verificar que un usuario es eliminado.
+	 * @author ALBERTO
+	 */
+	@Test
+	public void testEliminarUsuarios() {
+		boolean resultado = listaUsuarios.eliminarUsuario(3);
+		assertFalse(resultado);
+
+	}
 
 }
