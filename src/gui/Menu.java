b2ce06f153e033df3ed8be6d995fd589c543d5fd
Virginia <virgitb@gmail.com>
2021-03-22 09:37:16 +0100

    creado submenu3, menu modificado y main modificado

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -1,26 +1,26 @@
 package gui;
 
+/**
+ * @author Tangerine - Luis Rojo - modificado por Virginia Turina
+ *
+ */
 public class Menu {
 
+    /**
+     * Clase que muestra por pantalla el menú principal de nuestro videoclub "Movieflix"
+     */
     public static void mostrarMenu() {
-        System.out.println(" ");
+    	System.out.println(" ");
         System.out.println(" MOVIEFLIX");
         System.out.println(" ");
         System.out.println("************************************************");
         System.out.println("*  Por favor, escoge una opcion: ");
-        System.out.println("*    1.-Dar de alta usuario");
-        System.out.println("*    2.-Eliminar usuario");
-        System.out.println("*    3.-Modificar usuario");
-        System.out.println("*    4.-Añadir película");
-        System.out.println("*    5.-Eliminar película");
-        System.out.println("*    6.-Modificar película");
-        System.out.println("*    7.-Listado de películas");
-        System.out.println("*    8.-Listado de películas por categoría");
-        System.out.println("*    9.-Películas mejor valoradas");
-        System.out.println("*    0.-Terminar sesión");
+        System.out.println("*    1.-Gestión de películas");
+        System.out.println("*    2.-Gestión de usuarios");
+        System.out.println("*    3.-Otras Opciones");
+        System.out.println("*    4.-Terminar sesión");
         System.out.println("************************************************");
         System.out.println(" ");
         System.out.print(">> ");
-
     }
 }
