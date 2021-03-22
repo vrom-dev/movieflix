4bb49fa572bbe8ae666790f1b5ce33c33c035485
Vic Romero <vromerocomino@gmail.com>
2021-03-22 10:25:13 +0100

    Creada Clase SeleccionOpciones y refactorizacion de submenus

---
+++ src/control/SeleccionOpciones.java
@@ -0,0 +1,137 @@
+package control;
+
+import gui.Menu;
+import services.PeliculasServices;
+import services.UsuariosServices;
+import utilidades.LecturaDatos;
+
+/**
+ * Crea una clase SeleccionOpciones que muestra el menú.
+ * @param peliculas objeto PeliculasServices
+ * @param usuarios objeto UsuariosServices
+ * Con un DO-WHILE mostramos el menu principal siempre hasta que no se cumpla la condición
+ * Mostramos las diferentes opciones con SWITCH-CASE  y añadimos diferentes SWITCH-CASE anidados para crear los submenus.
+ */
+
+public class SeleccionOpciones {
+	public static void menuPrincipal(PeliculasServices peliculas, UsuariosServices usuarios) {
+		boolean seguir = true;
+		
+		do {
+			 Menu.mostrarMenu();
+			 switch(LecturaDatos.leerInt()) {
+			  case 1: // GESTION PELÍCULAS
+				  menuPeliculas(peliculas);
+				  break;
+				  
+			  case 2: // GESTION USUARIOS
+				  menuUsuarios(usuarios);
+				  break;
+			  
+			  case 3: // OTROS
+				  menuOtros(peliculas, usuarios);
+				  break;
+			  
+			  case 4: // SALIR
+				  seguir = false;
+				  break;
+			  default:
+				  System.out.println("La opción escogida no es válida. Escoge otra opción");
+				  break;
+			 }
+		} while (seguir);
+	}
+	
+	public static void menuUsuarios(UsuariosServices usuarios) {
+		boolean seguir = true;
+		do {
+			Menu.subMenuUsuarios();
+			switch (LecturaDatos.leerInt()) {
+			case 1:
+				/** ALTA DE USUARIOS */
+				usuarios.addUsuario();
+				break;
+
+			case 2:
+				/** ELIMINAR USUARIO */
+				usuarios.eliminarUsuario();
+				break;
+
+			case 3:
+				/** MODIFICAR USUARIO */
+				usuarios.modificarUsuario();
+				break;
+
+			case 4:
+				/** VOLVER AL MENU PRINCIPAL */
+				seguir = false;
+				break;
+			default:
+				System.out.println("La opción escogida no es válida. Escoge otra opción");
+				break;
+			}
+		} while (seguir);	
+	}
+	
+	public static void menuPeliculas(PeliculasServices peliculas) {
+		boolean seguir = true;
+		do {
+			Menu.subMenuPeliculas();
+			switch (LecturaDatos.leerInt()) {
+			case 1:
+				/** AÑADIR PELICULA */
+				peliculas.addPelicula();
+				break;
+
+			case 2:
+				/** ELIMINAR PELICULA */
+				peliculas.eliminarPelicula();
+				break;
+
+			case 3:
+				/** MODIFICAR PELICULA */
+				peliculas.modificarPelicula();
+				break;
+
+			case 4:
+				/** VOLVER AL MENU PRINCIPAL */
+				seguir = false;
+				break;
+			default:
+				System.out.println("La opción escogida no es válida. Escoge otra opción");
+				break;
+			}
+		} while (seguir);	
+	}
+	
+	public static void menuOtros(PeliculasServices peliculas, UsuariosServices usuarios) {
+		boolean seguir = true;
+		do {
+			Menu.subMenuOtros();
+			switch (LecturaDatos.leerInt()) {
+			case 1:
+				/** LISTADO DE TODAS LAS PELÍCULAS */
+				peliculas.listarPeliculas();
+				break;
+
+			case 2:
+				/** LISTADO DE PELÍCULAS POR CATEGORIA */
+				peliculas.listarPorCategoria();
+				break;
+				
+			case 3:
+				/** LISTADO DE USUARIOS */
+				usuarios.listarUsuarios();
+				break;
+
+			case 4:
+				/** VOLVER AL MENU PRINCIPAL */
+				seguir = false;
+				break;
+			default:
+				System.out.println("La opción escogida no es válida. Escoge otra opción");
+				break;
+			}
+		} while (seguir);	
+	}
+}
--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -23,4 +23,54 @@ public class Menu {
         System.out.println(" ");
         System.out.print(">> ");
     }
+    
+    public static void subMenuUsuarios() {
+	    System.out.println(" ");
+	    System.out.println(" GESTIONAR USUARIOS");
+	    System.out.println(" ");
+	    System.out.println("************************************************");
+	    System.out.println("*  Por favor, escoge una opcion: ");
+	    System.out.println("*    1.-Dar de alta usuario");
+	    System.out.println("*    2.-Eliminar usuario");
+	    System.out.println("*    3.-Modificar usuario");
+	    System.out.println("*    4.-Volver al menú principal");
+	    System.out.println("************************************************");
+	    System.out.println(" ");
+	    System.out.print(">> ");
+    }
+    
+    public static void subMenuPeliculas() {
+        System.out.println(" ");
+        System.out.println(" GESTIONAR PELICULAS");
+        System.out.println(" ");
+        System.out.println("************************************************");
+        System.out.println("*  Por favor, escoge una opcion: ");
+        System.out.println("*    1.-Añadir película");
+        System.out.println("*    2.-Eliminar película");
+        System.out.println("*    3.-Modificar película");
+        System.out.println("*    4.-Volver al menú principal");
+        System.out.println("************************************************");
+        System.out.println(" ");
+        System.out.print(">> ");
+    }
+    
+    /**
+     * @author Tangerine (Virginia Turina)
+	 * Clase que muestra por pantalla el submenu "OTRAS OPCIONES"
+	 */
+    
+    public static void subMenuOtros() {
+		System.out.println(" ");
+        System.out.println(" OTRAS OPCIONES");
+        System.out.println(" ");
+        System.out.println("************************************************");
+        System.out.println("*  Por favor, escoge una opcion: ");
+        System.out.println("*    1.-Listado de películas");
+        System.out.println("*    2.-Listado de películas por categoría");
+        System.out.println("*    3.-Listado de usuarios");
+        System.out.println("*    4.-Volver al menu principal");
+        System.out.println("************************************************");
+        System.out.println(" ");
+        System.out.print(">> ");    	
+    }
 }
